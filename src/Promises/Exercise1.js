3bf1f34485c0022c03a62886ee129793ea566271
Kyrie_Eleison <71424064+skynet6969@users.noreply.github.com>
2022-11-02 11:11:26 +0800

    Update Exercise1.js

--- src/Promises/Exercise1.js
+++ src/Promises/Exercise1.js
@@ -1,7 +1,33 @@
+// const display = () => {
+//   fetch('https://jsonplaceholder.typicode.com/users')
+//     .then((res) => res.json())
+//     .then((data) => console.log(data))
+//     .catch((err) => console.log(err))
+// }
+// export default display
 const display = () => {
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err))
+  const parse = function (response) {
+    const element = document.querySelector('.js-names')
+    element.innerHTML = JSON.parse(response).map(element => element.name).join(',')
+    console.log(response)
+  }
+  const errorHandler = function () {
+    console.log('fuck')
+  }
+  const myPromise = new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest()
+    request.onreadystatechange = function () {
+      if (this.status === 200 && this.readyState === 4) {
+        resolve(this.response)
+      }
+    }
+    request.onerror = function () {
+      reject(new Error(this.statusText))
+    }
+    request.open('GET', 'http://jsonplaceholder.typicode.com/users').request.send()
+    request.send()
+  }
+  ).then(parse).catch(errorHandler)
+  return myPromise
 }
-export default display
+display()
