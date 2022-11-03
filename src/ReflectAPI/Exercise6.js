d2c49ae1fe610a793211446c3056ccf45f743bd7
Kyrie_Eleison <71424064+skynet6969@users.noreply.github.com>
2022-11-03 14:46:01 +0800

    Update Exercise6.js

--- src/ReflectAPI/Exercise6.js
+++ src/ReflectAPI/Exercise6.js
@@ -1,21 +1,21 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-let response = function() {
-    let target = {};
-    let key = 'status';
-    let attributes = {
-    value: 200,
-    writable: true,
-    configurable: true
-};
+const display = () => {
+  // eslint-disable-next-line no-unused-vars
+  const response = (function () {
+    const target = {}
+    const key = 'status'
+    const attributes = {
+      value: 200,
+      writable: true,
+      configurable: true
+    }
     Reflect.defineProperty(
-    target,
-    key,
-    attributes
-);
-return target;
-}();
+      target,
+      key,
+      attributes
+    )
+    return target
+  }())
 
-let display = () => 
-console.log("is response configurable? i don't know")
-
+  console.log("is response configurable? i don't know")
+}
 export default display
