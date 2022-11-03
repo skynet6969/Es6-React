f2238f81448397396e11dd742ca193517e51b7b7
Kyrie_Eleison <71424064+skynet6969@users.noreply.github.com>
2022-11-03 13:13:07 +0800

    Update Exercise2.js

--- src/FunctionScope_BlockScope_Constants/Exercise2.js
+++ src/FunctionScope_BlockScope_Constants/Exercise2.js
@@ -1,16 +1,20 @@
-function display () {
+const display = () => {
   const guessMe1 = 1
-  const guessMe2 = 2
+  // eslint-disable-next-line prefer-const
+  let guessMe2 = 2
 
   {
-    const guessMe2 = 3
+    // eslint-disable-next-line prefer-const
+    let guessMe2 = 3
     console.log(guessMe1, guessMe2) // (A) 1 3
     console.log(guessMe1, guessMe2) // (B) 1 3
   }
   console.log(guessMe1, guessMe2); // (C) 1 2
   (() => {
-    const guessMe1 = 5
-    const guessMe2 = 6
+    // eslint-disable-next-line no-var
+    var guessMe1 = 5
+    // eslint-disable-next-line prefer-const
+    let guessMe2 = 6
     console.log(guessMe1) // (D) 5
     console.log(guessMe1, guessMe2) // (E) 5 6
   })()
