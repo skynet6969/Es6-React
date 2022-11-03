696c47d2b1d13c5c5421694724738450e20a976b
Kyrie_Eleison <71424064+skynet6969@users.noreply.github.com>
2022-11-03 14:33:44 +0800

    Update Exercise2.js

--- src/Promises/Exercise2.js
+++ src/Promises/Exercise2.js
@@ -1,48 +1,36 @@
-document.body.innerHTML = `
-<input type="text" class="js-textfield" />
-<button class="js-button">
-Enable the Textfield
-</button>` +
-document.body.innerHTML
-const enableFields = function () {
-  document.querySelector('.js-textfield')
-    .removeAttribute('disabled')
-  document.querySelector('.js-button')
-    .removeAttribute('disabled')
-}
-const disableFields = function () {
-  document.querySelector('.js-textfield')
-    .setAttribute('disabled', true)
-  document.querySelector('.js-button')
-    .setAttribute('disabled', true)
-}
-const parse = function (response) {
-  const element = document.querySelector('.js-names')
-  element.innerHTML =
-  JSON.parse(response)
-    .map(element => element.name)
-    .join(',')
-  enableFields()
-}
-const errorHandler = function () {
-  console.log('error')
-  enableFields()
-}
-new Promise(function (resolve, reject) {
-  disableFields()
-  const request = new XMLHttpRequest()
-  request.onreadystatechange = function () {
-    if (this.status === 200 && this.readyState === 4) {
-      resolve(this.response)
+const display = () => {
+  const enableFields = function () {
+    document.querySelector('.js-textfield').removeAttribute('disabled')
+    document.querySelector('.js-button').removeAttribute('disabled')
+  }
+  const disableFields = function () {
+    document.querySelector('.js-textfield').setAttribute('disabled', true)
+    document.querySelector('.js-button').setAttribute('disabled', true)
+  }
+  const parse = function (response) {
+    const element = document.querySelector('.js-names')
+    element.innerHTML = JSON.parse(response).map(element => element.name).join(',')
+    enableFields()
+  }
+  const errorHandler = function () {
+    console.log('error')
+    enableFields()
+  }
+  const myPromise = new Promise((resolve, reject) => {
+    disableFields()
+    const request = new XMLHttpRequest()
+    request.onreadystatechange = function () {
+      if (this.status === 200 && this.readyState === 4) {
+        resolve(this.response)
+      }
     }
+    request.onerror = function () {
+      reject(new Error(this.statusText))
+    }
+    request.open('GET', 'http://erroneousurl.com/users')
+    request.send()
   }
-  request.onerror = function () {
-    reject(new Error(this.statusText))
-  }
-  request.open(
-    'GET',
-    'http://erroneousurl.com/users'
-  )
-  request.send()
-}).then(parse)
-  .catch(errorHandler)
+  ).then(parse).catch(errorHandler)
+  return myPromise
+}
+display()
