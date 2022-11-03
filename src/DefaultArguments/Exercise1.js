ca5c55216ce10e34bf8943e386069bda301bdca1
Kyrie_Eleison <71424064+skynet6969@users.noreply.github.com>
2022-11-03 13:35:49 +0800

    Update Exercise1.js

--- src/DefaultArguments/Exercise1.js
+++ src/DefaultArguments/Exercise1.js
@@ -2,8 +2,6 @@ const display = () => {
   function executeCallback (callback, delay = 1000) {
     setTimeout(callback, delay)
   }
-
-  executeCallback(() => console.log('done'))
+  executeCallback(() => console.log('done'), 2000)
 }
-
 export default display
