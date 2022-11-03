cb932b34b9e685c6dbd5b65956bd1cef9281f252
Kyrie_Eleison <71424064+skynet6969@users.noreply.github.com>
2022-11-03 14:19:59 +0800

    Update Exercise2.js

--- src/Iterators/Exercise2.js
+++ src/Iterators/Exercise2.js
@@ -1,7 +1,9 @@
 const display = () => {
   const getCountdownIterator = function * () {
     let i = 10
-    while (i > 0) { yield --i }
+    while (i > 0) {
+      yield --i
+    }
   }
   console.log([...getCountdownIterator()])
 }
